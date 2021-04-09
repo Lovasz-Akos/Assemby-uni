1ce3f5686e22374224fee53964572d54e397c4f4
Lovász Ákos <akos4x44@gmail.com>
2021-04-09 09:58:12 +0200

    fixed template file

--- template.asm
+++ template.asm
@@ -6,11 +6,6 @@ assume cs:code, ds:code, es:nothing, ss:nothing
 
 
 start:
-	mov	ax,3					;80x25
-	int	10h						;vid bios
-	mov ah,9					;string ki¡r s
-	mov dx,offset tajeko		;a sz”veg maga
-	
 	mov ah,4c					;kil‚p‚s
 	int 21h						;dos kil‚p‚s
 
